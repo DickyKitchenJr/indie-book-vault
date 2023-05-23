dfdaa9f3f84a1c6c0a10d85a61f00dcea2db4df0
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-23 13:47:12 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from '../styles/header.module.css';
-import BookVault from '../assets/images/book-vault-small.webp';
+import BookVault from '../assets/images/book-vault.webp';
 
 function Header() {
   return (
     <>
     <header className={styles.header}>
-        <h1 className={styles.h1}> Indie Book Vault</h1> 
-        {/* TODO: Add img? */}
+        <div className={styles.centerVertical}>
+          <h1 className={styles.h1}>Indie<br/>Book<br/>Vault</h1> 
+        </div>
+        <img className={styles.img} src={BookVault} alt="vault of books" />
     </header>
     </>
   )
