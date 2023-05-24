ed719bbdd6acb6f8236d4cec8b702ac1ec389fb3
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-24 14:15:18 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './global.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingPage from './pages/LandingPage.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+import FaqPage from './pages/FaqPage'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "faq",
+    element: <FaqPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
