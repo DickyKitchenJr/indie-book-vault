6e72862dd7a214828599c295f8f53f2375b16338
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-24 14:15:21 -0400

    Create ErrorPage.jsx

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link, useRouteError } from 'react-router-dom'
+import styles from "../styles/error-page.module.css";
+
+function ErrorPage() {
+  const error = useRouteError();  
+  return (
+    <>
+    <div className={styles.errorBackground}>
+        <div className= {styles.errorMessage}>
+         <h2 className={styles.center}>It appears an error has occured: <i>{error.statusText || error.message}</i></h2>
+        </div> 
+
+        <div className={styles.errorMessage}>
+         <p className={styles.p}>Oh no! It seems you've gotten lost in the book vault!</p>
+         <p className={styles.p}>Don't worry, we'll get you out.</p>
+         <p className={styles.p}>Head back to the <Link to={'/'} className={styles.link}>home page</Link>.</p> 
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default ErrorPage
