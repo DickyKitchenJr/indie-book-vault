0da039e386ad83a938c5e92579a74800c50e4ccb
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-25 11:05:00 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LandingPage from './pages/LandingPage.jsx'
 import ErrorPage from './pages/ErrorPage.jsx'
 import FaqPage from './pages/FaqPage'
+import ContactPage from './pages/ContactPage'
+import AboutPage from './pages/AboutPage'
 
 const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ const router = createBrowserRouter([
   {
     path: "faq",
     element: <FaqPage />
+  },
+  {
+    path: "about",
+    element: <AboutPage />
+  },
+  {
+    path: "contact",
+    element: <ContactPage />
   }
 ])
 
