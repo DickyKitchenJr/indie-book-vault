d6d61b06ef8b1612cde6d32459a691d696519f68
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-25 16:48:02 -0400

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GenreTabs from '../components/GenreTabs';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 import styles from '../styles/landing-page.module.css';
 
 function LandingPage() {
@@ -14,6 +15,7 @@ function LandingPage() {
         <h2 className={styles.h2}>Pick A Genre</h2>
         <GenreTabs />
         </main>
+        <Footer />
       </div> 
     </>
   )
