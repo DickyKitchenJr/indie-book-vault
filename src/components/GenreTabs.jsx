8b81e8502c377fd248c9dc0a16969e13ad12abd1
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-30 17:11:02 -0400

    Update GenreTabs.jsx
    added key to link for mapping purposes

--- src/components/GenreTabs.jsx
+++ src/components/GenreTabs.jsx
@@ -12,18 +12,19 @@ import Western from '../assets/images/western-tab.webp';
 import React from 'react';
 import styles from '../styles/genre-tabs.module.css';
 import { Link } from 'react-router-dom';
+import { key } from 'localforage';
 
 // TODO: Randomize the presentation of the tabs on each page load; maybe put each tab object in another object with a num: number and then use math Random and math Floor to pick which genre is shown in which order?
 function GenreTabs() {
   const tabs = [{src: Children, alt: "Children's", link: "childrens"}, {src: Comedy, alt: "Comedy", link:"comedy"}, {src: Drama, alt: "Drama", link:"drama"}, {src: Dystopian, alt: "Dystopian", link:"dystopian"}, {src: Fantasy, alt: "Fantasy", link:"fantasy"}, {src: Horror, alt: "Horror", link:"horror"}, {src: NonFiction, alt: "Non-Fiction", link:"nonfiction"}, {src: Romance, alt:"Romance", link:"romance"}, {src: SciFi, alt: "Sci-Fi", link:"scifi"}, {src: Thriller, alt: "Thriller", link:"thriller"}, {src: Western, alt: "Western", link:"western"}];
+  
   return (
     <>
     <div className={styles.tabs}>
       {tabs.map(tab =>{
-        return <Link to={`/${tab.link}`}><img src={tab.src} alt={`${tab.alt} genre tab`} className={styles.img}/></Link>
+        return <Link to={`/${tab.link}`} key={tab.alt}><img src={tab.src} alt={`${tab.alt} genre tab`} className={styles.img}/></Link>
       })}  
     </div>
-      
     </>
   )
 }
