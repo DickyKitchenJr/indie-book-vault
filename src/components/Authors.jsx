e7ccee65a2833143d11444cf8ffdf48d87047cfe
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-30 17:10:08 -0400

    Create Authors.jsx
    added applicants who have submitted the form to main authors array

---
+++ src/components/Authors.jsx
@@ -0,0 +1,48 @@
+export const Authors = [
+{
+        firstName: "Dicky",
+        lastName: "Kitchen Jr",
+        website: "https://dickykitchenjr.com/",
+        socialMedia: ["https://www.instagram.com/dicky_kitchen_jr/"],
+        umbrellaGenre: ["thriller", "childrens"],
+        subGenre: ["Psychological Thriller", "Domestic Thriller", "Children's Tongue Twister"],
+        bio:"Introducing Dicky Kitchen Jr., a versatile author whose captivating tales encompass heart-stopping thrillers starring ‘The Average Man’ and enchanting children's stories, such as 'The True Tale of Peter Piper,' a whimsical reimagination of the classic tongue twister. With a background in healthcare as a Physical Therapist Assistant and a self-taught front-end developer, Dicky's diverse experiences infuse authenticity into his vividly imagined worlds. As a loving husband and father, he embraces the art of storytelling out of pure passion, evident in his debut novel that earned him a coveted spot as a top 10 finalist in the ‘2020 New Author’ contest by Balancing Books and Bottles. Praised with a remarkable 4.8 out of 5 star rating on Amazon, Dicky is not only a master storyteller but also a devoted supporter of the writing community, actively engaging with fellow authors on his discord server ‘BookInd’.",
+    },
+    {
+        firstName: "Elisa",
+        lastName: "Menz",
+        website: "www.elisamenz.com",
+        socialMedia: ["https://www.instagram.com/mayaseranawriter/"],
+        umbrellaGenre: ["fantasy", "romance"],
+        subGenre: ["Urban Fantasy", "Romantic Comedy", "High Fantasy"],
+        bio:"Elisa Menz is a self-taught writer and a fantasy literature fan. Born in Bolivia, she grew up surrounded by mythical creatures and supernatural folklore, through local writers and storytellers. Inspired by those tales, she started writing her own stories at a young age.<br/>Now living in Chile, she works full time as a writer, while dawdling in video game programming, comic design, and alternative sources of storytelling. A member of the Chilean Tolkien Society and an avid reader of fantasy classics, her work is heavily influenced by authors like David Eddings, Raymond E. Feist, and Robin Hobb.<br/>Her stories are full of witty, sweet, and hilarious characters, always with a pinch of magic.",
+    },
+   {
+        firstName: "Brittany",
+        lastName: "Putzer",
+        website: "www.brittanyputzer.com",
+        socialMedia: ["https://linktr.ee/brittany.putzer"],
+        umbrellaGenre: ["romance"],
+        subGenre: ["Rom Com", "Paranormal Romance", "Adventure", "Women's Fiction"],
+        bio:"Hi! My name is Brittany and I'm addicted to romance books. I am married to my high school sweetheart, I have 2 wild boys, 3 crazy cats, a scaled murderer, fur potato, and I live in sunny Florida.<br/>I read to escape life for awhile. Although I enjoy page surfing, my passion is writing unique tales with shocking twists and multiple love interests. In 2020 I published my first book and now I have 4 out with 125 paperbacks sold, 360 eBooks delivered, and over 13,000 pages turned on KU!<br/>I’m a mood reader and writer. So the first 3 books I published are clean YA romances with sweet heat, but my newest release is a HIGH heat HEA with wolf shifters.<br/>Within all my books you can expect:<br/>Steam<br/>Unexpected plot twists<br/>Relatable characters<br/>Strong female characters<br/>Encouragement through troubling times",
+    },
+    {
+        firstName: "Stephen",
+        lastName: "Mills",
+        website: "www.mayhemmills.com",
+        socialMedia: ["IG: Stephen.mills.writes", "FB: PursuitOfMayhem (Stephen Mills)"],
+        umbrellaGenre: ["horror", "fantasy"],
+        subGenre: ["Dark Urban Fantasy", "Dark Historical Fantasy"],
+        bio:"A soggy native of Washington State, Stephen Mills escaped as soon as he came to his senses by joining the US Navy. Many adventures ensued, some of which Stephen remembers fondly, and after tricking an amazing woman into marrying him he left the Naval Service. An undiagnosed yet temporary bout of mild insanity ensued in which Stephen convinced his wife to move back home to Washington. Realizing their mistake, they then found themselves in the small burg of Monument, Colorado...unfortunately Colorado is cold and snowy, so after braving the Rocky Mountains for almost a decade, they decided to enjoy the hellish heat of Phoenix before finally settling near Nashville, Tennessee.<br/>Stephen's most enjoyable hobbies are also those that are decidedly antisocial, such as hiking and fishing. It is difficult to put a finger on why he decided to begin writing books but it probably has something to do with sharing his nightmares with others.<br/>Thankfully, being an author isn't a team sport, so now you can enjoy his unique brand of dark humor and gory adventure in every book.",
+    },
+    {
+        firstName: "Miriam A",
+        lastName: "Maddock",
+        website: "https://mamaddockauthor.ie",
+        socialMedia: ["Instagram", "Facebook", "Goodreads", "Bookbub", "Tik Tok"],
+        umbrellaGenre: ["fantasy"],
+        subGenre: ["Historical Fantasy Fiction"],
+        bio:"I am an Irish author, currently living in Co. Meath. I am due to release book Two of my historical fantasy fiction epic saga - The Sixth Amulet- this coming June. While Book Two - The Moon Chasers- is being proofed, the audio for Book One- The Sixth Amulet - is currently in production with a view to release this July or August.",
+    },
+]
+    
