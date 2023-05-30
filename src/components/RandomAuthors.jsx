58204553490bbfae01c435254226e638efaceebc
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-30 17:11:11 -0400

    Create RandomAuthors.jsx

---
+++ src/components/RandomAuthors.jsx
@@ -0,0 +1,7 @@
+// PURPOSE: take all authors in each genre and randomize them into a new array of objects
+const randomAuthor = (Authors) =>{
+    // PURPOSE: get total count of authors
+    let count = Authors.length;
+}
+
+export default randomAuthor;
