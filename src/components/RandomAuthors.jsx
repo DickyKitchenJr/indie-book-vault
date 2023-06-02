8f7464ad56d6eadc22e47c41f70f8c57c54fd137
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-06-02 15:11:35 -0400

    Update RandomAuthors.jsx

--- src/components/RandomAuthors.jsx
+++ src/components/RandomAuthors.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "../styles/genre-pages.module.css";
 import { Link } from "react-router-dom";
 
 // PURPOSE: take all authors in the genre and randomize them then display
 function RandomAuthors({ authors }) {
-  const shuffledList = authors;
+  let isModalOpen = useRef(false);
+  let authorID = useRef("");
 
   const randomizeAuthors = (genreAuthors) => {
     let count = genreAuthors.length;
@@ -25,7 +26,27 @@ function RandomAuthors({ authors }) {
     return genreAuthors;
   };
 
-  randomizeAuthors(shuffledList);
+  // PURPOSE: create randomized list of authors
+  const shuffledList = randomizeAuthors(authors);
+
+  // PURPOSE: assign a unique number to each author to be used when choosing which modal to show
+  let authorNumber = 0;
+  shuffledList.forEach(author => author.number = authorNumber++)
+
+  const handleClick = (authorUniqueID) =>{
+    if (!isModalOpen.current) {
+      isModalOpen.current =true;
+      authorID.current = authorUniqueID;
+      console.log(isModalOpen)
+      console.log(authorID.current)
+    } else {
+      isModalOpen.current =false;
+      authorID.current = "";
+      console.log(isModalOpen.current);
+      console.log(authorID.current);
+    }
+  }
+
 
   return (
     <>
@@ -43,93 +64,155 @@ function RandomAuthors({ authors }) {
           </p>
         </div>
       ) : (
-        shuffledList.map((author) => {
-          return (
-            <>
-              <div
-                className={styles.authorInfo}
-                key={
-                  shuffledList[shuffledList.indexOf(author)] + author.lastName
-                }
-              >
-                <p>
-                  Name: {author.firstName} {author.lastName}
-                </p>
-                <p>Genres:</p>
-                <ul>
-                  {author.subGenre.length === 1 ? (
-                    <li key={"0G" + author.lastName}>{author.subGenre[0]}</li>
-                  ) : (
-                    author.subGenre.map((genre) => {
-                      return (
-                        <li
-                          key={
-                            author.subGenre[author.subGenre.indexOf(genre)] +
-                            "G" +
-                            author.lastName
-                          }
-                        >
-                          {genre}
-                        </li>
-                      );
-                    })
-                  )}
-                </ul>
-                <p>
-                  Bio:{" "}
-                  {author.bio.length === 1
-                    ? author.bio[0]
-                    : author.bio.map((sentence) => {
-                        return (
-                          <>
+        <>
+          <div className={styles.authorsListed}>
+            {shuffledList.map((author) => {
+              return (
+                <>
+                  <div
+                    className={styles.authorBox}
+                    key={
+                      shuffledList[shuffledList.indexOf(author)] +
+                      author.lastName
+                    }
+                    onClick={() => handleClick(author.lastName + author.number)}
+                  >
+                    <h3>
+                      {author.firstName} {author.lastName}
+                    </h3>
+                    <p>
+                      {author.subGenre.length === 1 ? (
+                        <span key={"0G" + author.lastName}>
+                          ▪ {author.subGenre[0]}
+                        </span>
+                      ) : (
+                        author.subGenre.map((genre) => {
+                          return (
                             <span
                               key={
-                                author.bio[author.bio.indexOf(sentence)] +
-                                "B" +
+                                author.subGenre[
+                                  author.subGenre.indexOf(genre)
+                                ] +
+                                "G" +
                                 author.lastName
                               }
                             >
-                              {sentence}
+                              ▪ {genre}{" "}
                             </span>
-                            <br />
-                            <br />
-                          </>
-                        );
-                      })}
-                </p>
-                <p>Website: {author.website}</p>
-                <p>Social Media:</p>
-                <ul>
-                  {author.socialMedia.length === 1 ? (
-                    <li key={"0SM" + author.lastName}>
-                      {author.socialMedia[0]}
-                    </li>
-                  ) : (
-                    author.socialMedia.map((platform) => {
-                      return (
-                        <li
-                          key={
-                            author.socialMedia[
-                              author.socialMedia.indexOf(platform)
-                            ] +
-                            "SM" +
-                            author.lastName
-                          }
-                        >
-                          {platform}
-                        </li>
-                      );
-                    })
-                  )}
-                </ul>
-                <br />
-              </div>
-            </>
-          );
-        })
+                          );
+                        })
+                      )}
+                    </p>
+                    <p>
+                      Bio:{" "}
+                      {author.bio.length === 1
+                        ? author.bio[0]
+                        : author.bio.map((sentence) => {
+                            return (
+                              <>
+                                <span
+                                  key={
+                                    author.bio[author.bio.indexOf(sentence)] +
+                                    "B" +
+                                    author.lastName
+                                  }
+                                >
+                                  {sentence}
+                                </span>
+                                <br />
+                              </>
+                            );
+                          })}
+                    </p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+          {console.log('Yes this is working')}
+          {shuffledList ? console.log("This works too") : null}
+          {shuffledList ? <h1>This also works, must be your tenary operator</h1> : null}
+          {isModalOpen.current &&
+          authorID.current
+            ? console.log(`The current authorID is ${authorID.current}`)
+            : null}
+        </>
       )}
     </>
   );
 }
 
 export default RandomAuthors;
+
+{/* <div
+  className={styles.authorInfo}
+  key={shuffledList[shuffledList.indexOf(author)] + author.lastName}
+>
+  <p>
+    Name: {author.firstName} {author.lastName}
+  </p>
+  <p>Genres:</p>
+  <ul>
+    {author.subGenre.length === 1 ? (
+      <li key={"0G" + author.lastName}>{author.subGenre[0]}</li>
+    ) : (
+      author.subGenre.map((genre) => {
+        return (
+          <li
+            key={
+              author.subGenre[author.subGenre.indexOf(genre)] +
+              "G" +
+              author.lastName
+            }
+          >
+            {genre}
+          </li>
+        );
+      })
+    )}
+  </ul>
+  <p>
+    Bio:{" "}
+    {author.bio.length === 1
+      ? author.bio[0]
+      : author.bio.map((sentence) => {
+          return (
+            <>
+              <span
+                key={
+                  author.bio[author.bio.indexOf(sentence)] +
+                  "B" +
+                  author.lastName
+                }
+              >
+                {sentence}
+              </span>
+              <br />
+              <br />
+            </>
+          );
+        })}
+  </p>
+  <p>Website: {author.website}</p>
+  <p>Social Media:</p>
+  <ul>
+    {author.socialMedia.length === 1 ? (
+      <li key={"0SM" + author.lastName}>{author.socialMedia[0]}</li>
+    ) : (
+      author.socialMedia.map((platform) => {
+        return (
+          <li
+            key={
+              author.socialMedia[author.socialMedia.indexOf(platform)] +
+              "SM" +
+              author.lastName
+            }
+          >
+            {platform}
+          </li>
+        );
+      })
+    )}
+  </ul>
+  <br />
+</div>; */}
