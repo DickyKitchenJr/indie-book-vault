6397ad9ca5885afc6657635305fae8db5300c09c
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-06-03 18:17:45 -0400

    moved randomizeAuthors function into genre pages
    moved randomizeAuthors() out of RandomAuthors.jsx and into genre specific pages to prevent re-shuffling of component content when onClick occurs

--- src/pages/GenrePages/Childrens.jsx
+++ src/pages/GenrePages/Childrens.jsx
@@ -5,12 +5,39 @@ import styles from "../../styles/genre-pages.module.css";
 import RandomAuthors from "../../components/RandomAuthors";
 
 function Childrens({ authors }) {
+
+  const randomizeAuthors = (genreAuthors) => {
+    let count = genreAuthors.length;
+    let firstItemHolder;
+    let swappedItem;
+
+    // PURPOSE: while count is above 0
+    while (count) {
+      // PURPOSE: pick a random item and subtract 1 from count
+      swappedItem = Math.floor(Math.random() * count--);
+      // PURPOSE: temporarily hold the information about to be replaced by the swap
+      firstItemHolder = genreAuthors[count];
+      // PURPOSE: swap the current item with secondItem
+      genreAuthors[count] = genreAuthors[swappedItem];
+      // PURPOSE: put the replaced item where secondItem originally was
+      genreAuthors[swappedItem] = firstItemHolder;
+    }
+    return genreAuthors;
+  };
+
+  // PURPOSE: create randomized list of authors
+  const shuffledList = randomizeAuthors(authors);
+
+  // PURPOSE: assign a unique number to each author to be used when choosing which modal to show
+  let authorNumber = 0;
+  shuffledList.forEach((author) => (author.number = authorNumber++));
+
   return (
     <>
       <main className={styles.main}>
         <h1 className={styles.h1}>Children's</h1>
         <Nav />
-        <RandomAuthors authors={authors} />
+        <RandomAuthors authors={shuffledList} />
       </main>
       <Footer />
     </>
--- src/pages/GenrePages/Comedy.jsx
+++ src/pages/GenrePages/Comedy.jsx
@@ -5,16 +5,41 @@ import styles from "../../styles/genre-pages.module.css";
 import RandomAuthors from "../../components/RandomAuthors";
 
 function Comedy({ authors }) {
+
+  const randomizeAuthors = (genreAuthors) => {
+    let count = genreAuthors.length;
+    let firstItemHolder;
+    let swappedItem;
+
+    // PURPOSE: while count is above 0
+    while (count) {
+      // PURPOSE: pick a random item and subtract 1 from count
+      swappedItem = Math.floor(Math.random() * count--);
+      // PURPOSE: temporarily hold the information about to be replaced by the swap
+      firstItemHolder = genreAuthors[count];
+      // PURPOSE: swap the current item with secondItem
+      genreAuthors[count] = genreAuthors[swappedItem];
+      // PURPOSE: put the replaced item where secondItem originally was
+      genreAuthors[swappedItem] = firstItemHolder;
+    }
+    return genreAuthors;
+  };
+
+  // PURPOSE: create randomized list of authors
+  const shuffledList = randomizeAuthors(authors);
+
+  // PURPOSE: assign a unique number to each author to be used when choosing which modal to show
+  let authorNumber = 0;
+  shuffledList.forEach((author) => (author.number = authorNumber++));
+
   return (
     <>
-      <div>
         <main className={styles.main}>
           <h1 className={styles.h1}>Comedy</h1>
           <Nav />
-          <RandomAuthors authors={authors} />
+          <RandomAuthors authors={shuffledList} />
         </main>
         <Footer />
-      </div>
     </>
   );
 }
