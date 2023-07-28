dd64eecb03851764da9d3eecd7e2c7afbb973ab5
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-07-28 18:14:07 -0400

    Create HistoricalFiction.jsx
    added historical fiction to available genre tabs

---
+++ src/pages/GenrePages/HistoricalFiction.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
+import styles from "../../styles/genre-pages.module.css";
+import RandomAuthors from "../../components/RandomAuthors";
+import ScrollToTop from "../../helpers/ScrollToTop";
+import randomizeAuthors from "../../helpers/randomizeAuthors";
+
+function HistoricalFiction({ authors }) {
+  // PURPOSE: create randomized list of authors
+  const shuffledList = randomizeAuthors(authors);
+
+  // PURPOSE: assign a unique number to each author to be used when choosing which modal to show
+  let authorNumber = 0;
+  shuffledList.forEach((author) => (author.number = authorNumber++));
+
+  return (
+    <>
+      <ScrollToTop />
+      <div className={styles.fullPage}>
+        <header>
+          <h1 className={styles.h1}>Historical Fict.</h1>
+        </header>
+        <Nav />
+        <main className={styles.main}>
+          <RandomAuthors authors={shuffledList} />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default HistoricalFiction;
