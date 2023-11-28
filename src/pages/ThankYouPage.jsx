c26ca230f727fd5919908929feb34b2ef91eb6a7
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-11-28 14:17:26 -0500

    Update ThankYouPage.jsx
    - finished inputting the information for the thank you page

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -17,7 +17,30 @@ function ThankYouPage() {
           <br />
           <p className={styles.p}>
             Your Author Application has been submitted and you are now one step
-            closer to being listed on our site.
+            closer to being listed on Indie Book Vault.
+          </p>
+          <br />
+          <p className={styles.p}>
+            If you are an author applying without the ability or desire to list
+            us on your website, please make sure you go to our{" "}
+            <a
+              href="https://www.buymeacoffee.com/indiebookvault"
+              target="_blank"
+              rel="noreferrer"
+            >
+              donation page
+            </a>{" "}
+            to make your $15 (USD) annual membership donation, making sure to
+            use the author name used on the application in the name section of
+            the donation, as author applications that do not include an author
+            website for listing our link will not be processed until the
+            donation is made.
+          </p>
+          <br />
+          <p className={styles.p}>
+            If you did include an author website on your application, you are
+            all set and your application will be processed in the order it was
+            received.
           </p>
           <br />
           <p className={styles.p}>
@@ -26,10 +49,30 @@ function ThankYouPage() {
           </p>
           <br />
           <p className={styles.p}>
-            Once you receive the welcome email you will have 7 days to complete the required link on your website and the required social media promotion.
+            Once you receive the welcome email you will have 7 days to complete
+            the required link on your website and the required social media
+            promotion. (no required website link for authors who donated $15 for
+            an annual membership)
           </p>
           <br />
-          
+          <p className={styles.p}>
+            If you have any questions or want to verify that we have received
+            your application, feel free to reach out to us on{" "}
+            <a
+              href="https://www.instagram.com/indiebookvault/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+            .
+          </p>
+          <br />
+          <p className={styles.p}>
+            While you are here, you might as well go find your next great read
+            from one of our wonderful authors. So what are you waiting for? Go
+            check out other author's listings and feed your reading addiction!
+          </p>
         </main>
         <Footer />
       </div>
